ed551cd46eeea61a432d6ab16d129aa7fb9230f2
ajmal <ajmalcm22@gmail.com>
2024-08-26 14:49:05 +0530

    userActions done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
@@ -7,7 +6,6 @@ const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
